cd7b40267549d093bb4ed7cca9db0f19ca46bbfa
Dasha <nosikovadasha74@gmail.com>
2025-10-05 18:30:04 +0300

    задание 5.1

---
+++ task1.cs
@@ -0,0 +1,30 @@
+
+using System;
+
+class Program
+{ 
+    static int FindMax(int a, int b)
+    {
+        // Возвращаем большее число  
+        return a > b ? a : b;
+    }
+
+    static void Main()
+    {
+        int a, b;
+
+        Console.WriteLine("Введите первое число: ");
+        a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число: ");
+        b = int.Parse(Console.ReadLine());
+
+        int maxNumber = FindMax(a, b);
+
+        Console.WriteLine($"Наибольшее число: {maxNumber}");
+
+      
+    }
+}
+
+
